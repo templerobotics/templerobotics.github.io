b1c8d53a8a811ab96cf62460f4480775bc744385
Ryan Hodge <ryanhodge240@gmail.com>
2023-03-13 15:05:16 -0400

    Fixed video and copyright

--- src/pages/general/copyright/CopyrightStyles.tsx
+++ src/pages/general/copyright/CopyrightStyles.tsx
@@ -4,8 +4,13 @@ import { COLORS } from '../../../tools/Constants'
 
 export default class CopyrightStyles {
 	static readonly CopyrightContainer = styled.div`
-        background-color: ${COLORS.PRIMARY};
-        text-align: center;
-        height: 100%;
+		height: calc(var(--vh) * .02);
+		align-self: end;
+		background-color: ${COLORS.PRIMARY};
+		text-align: center;
+		width: 100%;
+		display: flex;
+    	justify-content: center;
+		align-items: center;
 	`
 }
