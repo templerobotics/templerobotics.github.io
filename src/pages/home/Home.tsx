529de01cefbd59dca8619ae15d403c5bdaca03aa
NasierF <119966231+NasierF@users.noreply.github.com>
2024-01-12 20:58:41 -0500

    git problems

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -12,7 +12,7 @@ import Button from '../general/button/Button'
 // Images
 import nasaLogo from '../../assets/pics/logos/nasa.png'
 import HOME_CAROUSEL_INFO from '../../data/HomeCarouselInfo'
-import TOwl from '../../assets/pics/logos/TransRoboOwl.svg'
+// import TOwl from '../../assets/pics/logos/TransRoboOwl.svg'
 const Home = (): React.ReactElement => {
 	return (
 		<Styles.HomeContainer>
@@ -35,26 +35,16 @@ const Home = (): React.ReactElement => {
 			</Styles.VideoSource>
 
 			<Styles.SectionHeader>
-				<h2>
-					<img src={TOwl} 
-						alt='Owl' 
-						style={{
-							width: '150px',   // Set the width of the image
-							height: '150px', // Set the height of the image
-							margin: '0 0 0 -100px',// Set the margin around the image
-							float: 'left',
-							// us a .docx
-
-						}} 
-					/>
-						We hope to see you at Temple Fest 2023!</h2>
-				<div className='button-container'>	
+				{/* <img src={TOwl} alt='Owl'/> */}
+				<h2>We hope to see you at Temple Fest 2023!</h2>
+				<div className='button-container'>
 					<Button text={'Sign up here!'} size={'large'} source={'https://forms.gle/rp6BY6h4doLkZQ2E8'}/>
-					<Button text='Join Teams Here!' source={'https://teams.microsoft.com/l/team/19%3aeaf903fd81cd48eba95d8e769ed78544%40thread.tacv2/conversations?groupId=8f78ecbb-62f3-4b2c-bda7-7488eca908ee&tenantId=716e81ef-b522-4473-8e31-10bd02ccf6e5'}/>
+					<Button text='Join Teams Here!' size={'large'} source={`https://teams.microsoft.com/l/team/19%3aeaf903f
+					d81cd48eba95d8e769ed78544%40thread.tacv2/conversations?groupId=8f78ecbb-62f3-4b2c-bda
+					7-7488eca908ee&tenantId=716e81ef-b522-4473-8e31-10bd02ccf6e5`}/>
 				</div>
 			</Styles.SectionHeader>
 
-			
 
 			<Carousel slideInfo={HOME_CAROUSEL_INFO}/>
 
