37bd2d0497dac6919494d0023aaeeba9a54dde35
Malin <kussimalin@gmail.com>
2025-01-12 21:00:00 -0500

    Changed Weekly meetings to TBD

--- src/data/EventsDatabase.tsx
+++ src/data/EventsDatabase.tsx
@@ -1,43 +1,70 @@
 import { EventObject } from '../tools/CustomTypes'
 
-// const semesterEnd = '12/11/23'
+const semesterEnd = '5/30/25'
 
 const EVENT_INFO: EventObject[] = [
-	{
-		title: 'General Body Meeting',
-		description: `Learn more about the club and what we do! If you have any interest at all,
-		we highly recommend you joining us!`,
-		date: new Date('9/4/2024'),
-		time: '5:00pm',
-		location: 'The Fish Bowl (first floor pf the engineering building)'
-	},
+	// {
+	// 	title: 'General Body Meeting',
+	// 	description: `Learn more about the club and what we do! If you have any interest at all,
+	// 	we highly recommend you joining us!`,
+	// 	date: new Date('1/11/2025'),
+	// 	time: '5:00pm',
+	// 	location: 'The Fish Bowl (first floor pf the engineering building)'
+	// },
 	{
 		title: 'Weekly Programming Meeting',
-		description: 'The weekly meeting for the programming sub-team. Take a look at the code for the robot and ',
-		date: new Date('9/11/2024'),
-		time: '5:00pm',
+		description: 'The weekly meeting for the programming sub-team. Take a look at the code for the robot',
+		date: new Date('TBD'),
+		time: 'TBD',
 		location: 'The ideas hub (second floor of the engineering building)',
-		weekly: true
-		// endDate: new Date(semesterEnd)
+		weekly: true,
+		endDate: new Date(semesterEnd)
 	},
 	{
 		title: 'Weekly Electrical Meeting',
 		description: 'The weekly meeting for the electrical sub-team. Talk about improved batteries, wiring, and more!',
-		date: new Date('9/9/2024'),
-		time: '6:00',
+		date: new Date('TBD'),
+		time: 'TBD',
 		location: 'The ideas hub (second floor of the engineering building)',
-		weekly: true
-		// endDate: new Date(semesterEnd)
+		weekly: true,
+		endDate: new Date(semesterEnd)
 	},
 	{
 		title: 'Weekly Mechanical Meeting',
 		description: 'The weekly meeting for the mechanical sub-team. Learn about the design aspects of robot and 3D model parts.',
-		date: new Date('9/11/2024'),
-		time: '4:00pm',
+		date: new Date('TBD'),
+		time: 'TBD',
 		location: 'The ideas hub (second floor of the engineering building)',
-		weekly: true
-		// endDate: new Date(semesterEnd)
+		weekly: true,
+		endDate: new Date(semesterEnd)
 	}
+	// {
+	// 	title: 'Weekly Programming Meeting',
+	// 	description: 'The weekly meeting for the programming sub-team. Take a look at the code for the robot',
+	// 	date: new Date('1/1/2025'),
+	// 	time: '5:00pm',
+	// 	location: 'The ideas hub (second floor of the engineering building)',
+	// 	weekly: true,
+	// 	endDate: new Date(semesterEnd)
+	// },
+	// {
+	// 	title: 'Weekly Electrical Meeting',
+	// 	description: 'The weekly meeting for the electrical sub-team. Talk about improved batteries, wiring, and more!',
+	// 	date: new Date('1/2/2025'),
+	// 	time: '6:00',
+	// 	location: 'The ideas hub (second floor of the engineering building)',
+	// 	weekly: true,
+	// 	endDate: new Date(semesterEnd)
+	// },
+	// {
+	// 	title: 'Weekly Mechanical Meeting',
+	// 	description: 'The weekly meeting for the mechanical sub-team. Learn about the design aspects of robot and 3D model parts.',
+	// 	date: new Date('1/2/2025'),
+	// 	time: '4:00pm',
+	// 	location: 'The ideas hub (second floor of the engineering building)',
+	// 	weekly: true,
+	// 	endDate: new Date(semesterEnd)
+	// }
 	// {
 	// 	title: 'Engineering Week Showcase',
 	// 	description: 'Meet us to see the robot in action and learn about the club!',
