bcc2455e6f3fa6660e2ed176032b33c23b6a78e6
Malin <kussimalin@gmail.com>
2025-01-21 13:45:26 -0500

    deleted commented lines

--- src/data/EventSignUpInfo.tsx
+++ src/data/EventSignUpInfo.tsx
@@ -1,12 +1,6 @@
 import { EventSignUpProps } from '../pages/events/EventSignUpStyles'
 
 const EVENT_SIGNUP_INFO: EventSignUpProps[] = [
-	// {
-	// 	title: 'Robotics Sign Up ',
-	// 	description: 'Sign up for the club!',
-	// 	src: 'https://forms.gle/pPHGRk5EkNQ7nJHd9'
-	// }
-	// {
 	// 	title: 'Build Day Sign Up',
 	// 	description: 'Sign up for the outreach build days!',
 	// 	src: 'https://docs.google.com/forms/d/e/1FAIpQLSdOjuVGK6zYMWIRCSSR3LGqVTssxk2A5HxDehjSpYo6C3tGAw/viewform?usp=sf_link'
