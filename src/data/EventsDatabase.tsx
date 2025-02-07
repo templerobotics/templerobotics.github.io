e780741108f65dc880c38e5ff7b8a1d48ca1ee36
Malin <kussimalin@gmail.com>
2025-02-06 19:24:30 -0500

    Fixed max line error

--- src/data/EventsDatabase.tsx
+++ src/data/EventsDatabase.tsx
@@ -1,11 +1,12 @@
 import { EventObject } from '../tools/CustomTypes'
 
 const semesterEnd = '5/31/25'
-const descriptionForSDW = 'The goal of these classes is to equip you with the skills to model and design on a professional level, as well as create a common ground for all engineers to work together more efficiently and effectively'
+const descriptionForSDW1 = 'The goal of these classes is to equip you with the skills to model and design on a '
+const descriptionForSDW2 = 'professional level, as well as create a common ground for all engineers to work together more efficiently and effectively'
 const EVENT_INFO: EventObject[] = [
 	{
 		title: 'Weekly Solidworks Workshop',
-		description: descriptionForSDW,
+		description: descriptionForSDW1 + descriptionForSDW2,
 		date: new Date('2025-02-17T17:30:00'),
 		location: 'The ideas hub (second floor of the engineering building)',
 		weekly: true,
