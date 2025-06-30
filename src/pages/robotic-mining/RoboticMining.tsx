eb8cc0c3b9464bdde994a2e1b5b3013d5b085d3d
Malin Kussi <malin@malins-mbp.mynetworksettings.com>
2025-06-29 20:07:04 -0400

    Fixing trailing space error

--- src/pages/robotic-mining/RoboticMining.tsx
+++ src/pages/robotic-mining/RoboticMining.tsx
@@ -43,7 +43,6 @@ const RoboticMining = (): React.ReactElement => {
 
 			<Styles.SpaceBackground>
 				<Styles.MeetTheTeam className='meet-the-team-gallery' galleryInfo={ MEET_THE_TEAM } title={ 'Meet the Team' }/>
-	
 				<Styles.OutreachGallery>
 					<h2>Outreach and Events</h2>
 					<div className='outreach-slide'>
