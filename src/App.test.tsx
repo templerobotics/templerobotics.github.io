8083ac2d656689c16a3d0a38d112aee810376783
Malin Kussi <tur72969@temple.edu>
2025-10-02 12:13:41 -0400

    trying to fix deploy error and adding matchMedia to setupTests.ts file

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -2,21 +2,6 @@ import { render } from '@testing-library/react'
 import React from 'react'
 import App from './App'
 
-// Add this before your tests run
-Object.defineProperty(window, 'matchMedia', {
-  writable: true,
-  value: jest.fn().mockImplementation(query => ({
-    matches: false,
-    media: query,
-    onchange: null,
-    addListener: jest.fn(), // deprecated
-    removeListener: jest.fn(), // deprecated
-    addEventListener: jest.fn(),
-    removeEventListener: jest.fn(),
-    dispatchEvent: jest.fn(),
-  })),
-});
-
 test('renders learn react link', () => {
 	render(<App />)
 })
--- src/setupTests.ts
+++ src/setupTests.ts
@@ -5,3 +5,18 @@
  * learn more: https://github.com/testing-library/jest-dom
  */
 import '@testing-library/jest-dom'
+
+// Add this before your tests run
+Object.defineProperty(window, 'matchMedia', {
+  writable: true,
+  value: jest.fn().mockImplementation(query => ({
+    matches: false,
+    media: query,
+    onchange: null,
+    addListener: jest.fn(), // deprecated
+    removeListener: jest.fn(), // deprecated
+    addEventListener: jest.fn(),
+    removeEventListener: jest.fn(),
+    dispatchEvent: jest.fn(),
+  })),
+});
